66db83c845962eba21cf903eba000958ab9197d1
Danyllo Alarcao <danyllo.alarcao@hotmail.com>
2020-02-26 21:16:00 -0300

    testando dominio categorias

--- src/main/java/com/modelo/cursomc/resources/CategoriaResouces.java
+++ src/main/java/com/modelo/cursomc/resources/CategoriaResouces.java
@@ -1,17 +1,30 @@
 package com.modelo.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.modelo.cursomc.domain.Categoria;
+
 @RestController
-@RequestMapping(value="/Categorias")
+@RequestMapping(value="/categorias")
 public class CategoriaResouces {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String Lista() {
+	public List<Categoria> lista() {
 		
-		return "Funcionando";
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 		
 	}
 	
